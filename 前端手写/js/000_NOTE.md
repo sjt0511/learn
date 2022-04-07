e9d8b3ae88e7ba3dde2be53ab2b4af40b2a4c537
shijiangting <2628664034@qq.com>
2022-04-07 16:43:16 +0800

    .

--- 前端手写/js/000_NOTE.md
+++ 前端手写/js/000_NOTE.md
@@ -4,4 +4,6 @@
 
 ### 原型链
 
+实例有__proto__属性(隐式)，构造函数有prototype(显示)
+
 ![原型链](https://user-images.githubusercontent.com/23610322/162155112-060fcd76-7352-48e3-9cf8-e0750a4997c8.png)
--- 前端手写/js/004_instanceOf.js
+++ 前端手写/js/004_instanceOf.js
@@ -3,6 +3,10 @@
 // 步骤：
 //   1: 先取得当前类的原型，当前实例对象的原型链
 //   2: 一直循环（执行原型链的查找机制）
+// 取得当前实例对象原型链的原型链（proto = proto.__proto__，沿着原型链一直向上查找）
+// 如果 当前实例的原型链__proto__上找到了当前类的原型prototype，则返回 true
+// 如果 一直找到Object.prototype.__proto__ == null，Object的基类(null)上面都没找到，则返回 false
 const _004_instanceOf = {
-
+    myInstanceOf () {
+    }
 }
